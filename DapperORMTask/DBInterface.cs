e83cdca7db5fbcbb40bf7b85543ecc479a84b7eb
DESKTOP-F6NPFHB\sekul <sekulovski.atanas@gmail.com>
2022-12-18 04:09:37 +0100

    Add project files.

---
+++ DapperORMTask/DBInterface.cs
@@ -0,0 +1,154 @@
+﻿using Dapper;
+using DapperORMTask.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperORMTask
+{
+    static class DBInterface
+    {
+        private static String connectionString = "Server=localhost;Database=dapperschema;User=dapperuser;Password=dapperpass";
+        //SET FOREIGN_KEY_CHECKS = 0; ignoriraj foreign key check pri table update
+
+        //GET: Return all categories
+        public static List<Category> GetAllCategories()
+        {
+            
+            List<Category> categories;       
+
+            string getCategoriesQuery = "Select * from categories";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                
+                 categories = connection.Query<Category>(getCategoriesQuery).ToList();
+                
+            }
+
+            return categories;
+        }
+
+        //POST: Add a new category
+        public static void CreateCategory(String categoryName, String description, string picture)
+        {
+
+            string insertCategoryQuery = "INSERT INTO categories (CategoryName, Description, Picture) " +
+                "VALUES (@CategoryName, @Description, @Picture)";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+
+                connection.Execute(insertCategoryQuery, new {CategoryName = categoryName, Description = description, Picture = picture});
+            
+            }
+        }
+
+        //POST: Add a new product
+        public static void CreateProduct(Product product)
+        {
+
+            //Sigurno ima poubav i popregleden nacin?
+            string insertProductQuery = "INSERT INTO products(ProductName, SupplierID, CategoryID, " +
+                "QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, " +
+                "ReorderLevel, Discontinued, LastUserId, LastDateUpdated)" +
+                "VALUES (@ProductName, @SupplierID, @CategoryID, " +
+                "@QuantityPerUnit, @UnitPrice, @UnitsInStock, @UnitsOnOrder, " +
+                "@ReorderLevel, @Discontinued, @LastUserId, @LastDateUpdated)";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+
+                connection.Execute(insertProductQuery, product);
+
+            }
+        }
+
+        //POST: Add a new order
+        public static void CreateOrder(Order order)
+        {
+            string insertOrderQuery = "INSERT INTO orders(CustomerID, EmployeeID, OrderDate, " +
+                "RequiredDate, ShippedDate, ShipVia, Freight, ShipName, ShipAddress, ShipCity," +
+                "ShipRegion, ShipPostalCode, ShipCountry )" +
+                "VALUES (@CustomerID, @EmployeeID, @OrderDate, " +
+                "@RequiredDate, @ShippedDate, @ShipVia, @Freight, @ShipName, @ShipAddress, @ShipCity," +
+                "@ShipRegion, @ShipPostalCode, @ShipCountry)";
+
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+
+                connection.Execute(insertOrderQuery, order);
+
+            }
+        }
+
+        //POST: Add a new order_detail
+        public static void CreateOrderDetail(int orderId,int productId, float unitPrice, int quantity, float discount)
+        {
+            string insertCategoryQuery = "INSERT INTO order_details (OrderID, ProductID, UnitPrice, Quantity, Discount) " +
+                "VALUES (@OrderID, @ProductID, @UnitPrice, @Quantity, @Discount) ";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+
+                connection.Execute(insertCategoryQuery, new { OrderID = orderId, ProductID = productId, UnitPrice = unitPrice, Quantity = quantity, Discount = discount });
+                
+            }
+        }
+
+        //GET: Return orders sorted by date
+        public static List<Order> GetOrdersByDate()
+        {
+            string getOrderQuery = "SELECT * FROM orders ORDER BY OrderDate, RequiredDate, ShippedDate";
+            List<Order> orders;
+            using (var connection = new MySqlConnection(connectionString))
+            {
+
+                orders = connection.Query<Order>(getOrderQuery).ToList();
+
+            }
+            return orders;
+        }
+
+        //GET: Return products sorted by most sold
+        public static List<dynamic> GetProductsByMostSold()
+        {
+            string getProductQuery = "SELECT ProductID, ProductName, CategoryID, SUM(Quantity) " +
+                "FROM (SELECT p.ProductID, p.ProductName, p.CategoryID, od.Quantity " +
+                "FROM products AS p " +
+                "JOIN order_details AS od ON p.ProductID = od.ProductID) AS T GROUP BY ProductID " +
+                "ORDER BY SUM(Quantity) DESC ";
+           
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                var products = connection.Query(getProductQuery).ToList();
+
+                return products;
+            }
+
+        }
+
+        //GET: Return categories by most sold products in said categories.
+        public static List<dynamic> GetCategoriesByMostSold()
+        {
+            string getProductQuery = "SELECT ProductID, ProductName, CategoryID, SUM(Quantity) " +
+                "FROM (SELECT p.ProductID, p.ProductName, p.CategoryID, od.Quantity " +
+                "FROM products AS p " +
+                "JOIN order_details AS od ON p.ProductID = od.ProductID) AS T GROUP BY CategoryID " +
+                "ORDER BY SUM(Quantity) DESC ";
+
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                var categories = connection.Query(getProductQuery).ToList();
+
+                return categories;
+            }
+        }
+    }
+}
---
+++ DapperORMTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Dapper;
+using DapperORMTask;
+using DapperORMTask.Models;
+using MySql.Data.MySqlClient;
+
+
+// See https://aka.ms/new-console-template for more information
+
+
+//FOR TESTING
+//
+//Return all categories
+
+List<Category> categories = DBInterface.GetAllCategories();
+Console.WriteLine("--ALL CATEGORIES--");
+categories.ForEach(Console.WriteLine);
+
+
+// Create a single product and order object
+//
+
+Product product = new Product("TestSecondName",1,1,2,(float)3.14,2,4,2,0,4,DateTime.Now);
+Order order = new Order(1, 1, DateTime.Now, DateTime.Now, DateTime.Now, "Big boat", 12345.54m, "Biggest Boat", "Big Address", "Big City", "Big Region", "12345", "Big Country");
+
+
+// The following code is commented so as not to spam database during testing
+
+/* Then execute the INSERT INTO query via these methods.
+//
+DBInterface.CreateProduct(product);
+DBInterface.CreateOrder(order);
+
+// These functions require individual arguments instead of an entire object
+//
+DBInterface.CreateOrderDetail(5,2,(float)215.5,50,(float)20.3);
+DBInterface.CreateCategory("TestName", "TestDescription", "TestPictureURL");
+*/
+
+
+// Return orders, ordered by date
+//
+List<Order> orders = DBInterface.GetOrdersByDate();
+Console.WriteLine("--ALL ORDERS, SORTED BY DATE--");
+orders.ForEach(Console.WriteLine);
+
+// Return products, ordered by most sold
+//
+var products = DBInterface.GetProductsByMostSold();
+Console.WriteLine("--ALL PRODUCTS, SORTED BY MOST SOLD--");
+products.ForEach(Console.WriteLine);
+
+// Return categories, ordered by most sold products in that category
+//
+var orderedCategories = DBInterface.GetCategoriesByMostSold();
+Console.WriteLine("--ALL CATEGORIES, SORTED BY MOST SOLD PRODUCTS--");
+orderedCategories.ForEach(Console.WriteLine);
